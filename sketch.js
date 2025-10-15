0efdfda8bf43f81b912d307db4dbfb1fff245d96
TommasoBarazzetta <tommaso.barazzetta@mail.polimi.it>
2025-10-15 11:19:17 +0200

    inizio attività in classe con Mauri

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
+let table;
+
+
 function preload() {
   // put preload code here
+  table=loadTable("dataset.csv", "csv", "header")
 }
 
 function setup() {
+  console.log(table);
+  
   createCanvas(windowWidth, windowHeight);
+
   // put setup code here
   const message =
     "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
